7a4894404fc7b9d1763d51bca3db0841a55b42b5
Lyova Potyomkin <lyova.potyomkin@gmail.com>
2023-05-21 21:28:13 +0300

    Add verifier generation

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,10 +1,12 @@
 import '@typechain/hardhat';
 import '@nomiclabs/hardhat-ethers';
 import { HardhatUserConfig, task } from 'hardhat/config';
+
 import fs from 'fs';
 import path from 'path';
+import { spawnSync } from 'child_process';
 
-task("mimc7", "Generates the MiMC7 hasher contract").setAction(async () => {
+task("mimc7", "Generates the MiMC7 hasher bytecode").setAction(async () => {
     const { mimc7Contract } = require('circomlibjs');
     const contract = {
         contractName: 'Hasher',
@@ -18,10 +20,26 @@ task("mimc7", "Generates the MiMC7 hasher contract").setAction(async () => {
     return contract;
 });
 
+task("verifier", "Generates the verifier contract").setAction(async (_args, hre) => {
+    if (fs.existsSync('contracts/Verifier.sol')) {
+        console.log('Verifier.sol already exists, skipping');
+        return;
+    }
+    process.chdir(path.join(__dirname, 'circuits'));
+    console.log('Generating verifier contract...');
+    spawnSync('nargo', ['codegen-verifier']);
+    fs.renameSync('contract/plonk_vk.sol', '../contracts/Verifier.sol');
+    fs.rmdirSync('contract');
+    console.log('Done');
+});
+
 const config: HardhatUserConfig = {
-  solidity: {
-    version: "0.7.6"
-  },
+    solidity: {
+        version: "0.7.6",
+        settings: {
+            optimizer: { enabled: true },
+        }
+    },
 };
 
 export default config;
